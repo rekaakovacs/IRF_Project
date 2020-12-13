e435d9d90cd1dc28bca1e8873a3d45eaf68935fc
rekaakovacs <reka.kovacs8@stud.uni-corvinus.hu>
2020-12-13 19:01:20 +0100

    commit1

--- KaracsonyfaRendeles/KaracsonyfaRendeles/Form1.Designer.cs
+++ KaracsonyfaRendeles/KaracsonyfaRendeles/Form1.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonUj = new System.Windows.Forms.Button();
+            this.buttonTorles = new System.Windows.Forms.Button();
+            this.buttonFenyok = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // buttonUj
+            // 
+            this.buttonUj.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonUj.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonUj.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonUj.ForeColor = System.Drawing.Color.White;
+            this.buttonUj.Location = new System.Drawing.Point(12, 277);
+            this.buttonUj.Name = "buttonUj";
+            this.buttonUj.Size = new System.Drawing.Size(154, 65);
+            this.buttonUj.TabIndex = 1;
+            this.buttonUj.Text = "Új rendelés";
+            this.buttonUj.UseVisualStyleBackColor = false;
+            // 
+            // buttonTorles
+            // 
+            this.buttonTorles.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonTorles.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonTorles.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonTorles.ForeColor = System.Drawing.Color.White;
+            this.buttonTorles.Location = new System.Drawing.Point(12, 384);
+            this.buttonTorles.Name = "buttonTorles";
+            this.buttonTorles.Size = new System.Drawing.Size(154, 65);
+            this.buttonTorles.TabIndex = 2;
+            this.buttonTorles.Text = "Rendelés törlése";
+            this.buttonTorles.UseVisualStyleBackColor = false;
+            // 
+            // buttonFenyok
+            // 
+            this.buttonFenyok.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonFenyok.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonFenyok.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonFenyok.ForeColor = System.Drawing.Color.White;
+            this.buttonFenyok.Location = new System.Drawing.Point(12, 168);
+            this.buttonFenyok.Name = "buttonFenyok";
+            this.buttonFenyok.Size = new System.Drawing.Size(154, 65);
+            this.buttonFenyok.TabIndex = 3;
+            this.buttonFenyok.Text = "Elérhető fenyőink";
+            this.buttonFenyok.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 33.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(207, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(624, 55);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Karácsonyfa házhozszállítás";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(188, 103);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(818, 500);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(1032, 625);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonFenyok);
+            this.Controls.Add(this.buttonTorles);
+            this.Controls.Add(this.buttonUj);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonUj;
+        private System.Windows.Forms.Button buttonTorles;
+        private System.Windows.Forms.Button buttonFenyok;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
